b5683f7dbde9280055d7da3f248638fef0bf84fe
Alexander S. Green <alexander.s.green@gmail.com>
2020-01-11 21:38:39 +0000

    add example code

--- src/System/Environment/Typed.hs
+++ src/System/Environment/Typed.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE DataKinds        #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TemplateHaskell  #-}
 
 module System.Environment.Typed
     ( EnvVar(..)
@@ -9,19 +7,10 @@ module System.Environment.Typed
     , FromEnvVar(..)
     , lookupEnvVars
     , mkEnvVarsRecord
+    , Proxy(..)
     ) where
 
 import System.Environment.Typed.Internal
 
 envVarList :: (EnvVars args [(String,String)]) => p args -> EnvVarFunction args [(String,String)]
 envVarList = flip envVarsFunction id 
-
-type PATH = EnvVar "PATH" String
-type PWD  = EnvVar "PWD"  String
-
-type Environment = [ PATH , PWD ]
-
-mkEnvVarsRecord ''Environment
-
-lookupEnvironment :: IO EnvironmentRecord
-lookupEnvironment = lookupEnvVars (Proxy :: Proxy Environment) EnvironmentRecord
